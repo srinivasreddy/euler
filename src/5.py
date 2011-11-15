03cef98f6fa6462928f7b835a87fa8f193fd3b25
srinivas <thatiparthysreenivas@gmail.com>
2011-11-16 00:21:42 +0530

    added 5 th problem solution

---
+++ src/5.py
@@ -0,0 +1,22 @@
+"""
+The lcm and gcd functions 
+"""
+def lcm(a,b):
+    return a*b/gcd(a,b)
+
+def gcd(a,b):
+    while b>0:
+        a,b =b,a%b
+    return a
+
+
+"""
+smallest divisor for numbers between 1 ,2,3,.....20 is 
+their LCM.
+"""
+def min_divisor():
+    x=1
+    for i in range(1,21):
+        x=lcm(x,i)
+    print x
+
