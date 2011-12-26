377c7a8fd75704ed1d08c01809306397f3830067
srinivas <thatiparthysreenivas@gmail.com>
2011-12-26 12:38:57 +0530

    corrected solution 3

--- src/3.py
+++ src/3.py
@@ -4,7 +4,6 @@ import math
 function to generate infinite prime numbers!!!
 """
 def gen_prime():
-    yield 1
     yield 2
     x=3
     while True:
@@ -13,20 +12,24 @@ def gen_prime():
         x=x+2
 
 def is_prime(x):
-    for i in range(1,int(math.sqrt(x))+1,2):
+    for i in range(3,int(math.sqrt(x))+1,2):
         if(x%i==0):
             return False
     return True
 
 def largest_primefactor():
-    cache=math.sqrt(600851475143)
+    cache=int(math.sqrt(600851475143))
     temp=None
     for i in gen_prime():
         if(i<=cache and 600851475143%i==0):
            temp=i
+        elif(i>cache):
+            break
     return temp
 
 if __name__=="__main__":
+   print is_prime(100)
+   print is_prime(7)
    print largest_primefactor()
 
 
