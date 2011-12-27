97b8c39b2e3bb6cedc68e7f0a7975e1d9be4f118
srinivas <thatiparthysreenivas@gmail.com>
2011-12-28 05:01:02 +0530

    solved 19th problem

--- src/19.py
+++ src/19.py
@@ -1,5 +1,18 @@
 from datetime import  date
 
+
+"""
+I should also try to solve the other way,as day of week (sunday,monday....etc) 
+is handled by datetime...
+
+Have to think and write considering leap year and blah ,blah.....
+python handles days of week as follows:
+sunday --0
+monday --1
+.......
+.......
+saturday --6
+"""
 if __name__ =="__main__":
    total=0
    for year in range(1,101):
