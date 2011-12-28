a7598afb47daedb57fd6aeb941256ac4dedeb7eb
srinivas <thatiparthysreenivas@gmail.com>
2011-12-28 20:05:06 +0530

    sunday is 6 and monday is 0

--- src/19.py
+++ src/19.py
@@ -7,18 +7,18 @@ is handled by datetime...
 
 Have to think and write considering leap year and blah ,blah.....
 python handles days of week as follows:
-sunday --0
-monday --1
+monday --0
 .......
 .......
-saturday --6
+saturday --5
+sunday --6
 """
 if __name__ =="__main__":
    total=0
    for year in range(1,101):
        for month in range(1,13):
            d=date(1900+year,month,1)
-           if(d.weekday()==1):
+           if(d.weekday()==6):
               total=total+1
    print total
 
