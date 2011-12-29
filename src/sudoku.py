9bb4ec41a5a6615d7aa9d7673627dd9dbf08aa49
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2011-12-29 19:32:23 +0530

    clean-up in sudoku

--- src/sudoku.py
+++ src/sudoku.py
@@ -5,21 +5,24 @@ array=[[0,2,0,0,4,0,0,5,0],[0,0,0,7,2,6,0,0,0],[0,0,7,0,9,0,6,0,0],
 def find(llist,value):
     return value in llist
 
-while(0 in [array[i][j] for i in range(9) for j in range(9)]):
-    for i in range(1,10):
-	    for j in range(9):
-                count=0;
-                for k in range(9):
-                    if(array[j][k] ==0):
-			a=[array[j][l] for l in range(9)];
-			b=[array[l][k] for l in range(9)];
-			c=[array[l][m] for l in range((j-j%3),(j-j%3)+3) for m in range((k-k%3),(k-k%3)+3) ];
-			if(not find(a,i) and not find(b,i) and not find(c,i)):
+def sudoku():
+    while(0 in [array[i][j] for i in range(9) for j in range(9)]):
+          for i in range(1,10):
+	      for j in range(9):
+                  count=0;
+                  for k in range(9):
+                      if(array[j][k] ==0):
+			  a=[array[j][l] for l in range(9)];
+			  b=[array[l][k] for l in range(9)];
+			  c=[array[l][m] for l in range((j-j%3),(j-j%3)+3) for m in range((k-k%3),(k-k%3)+3) ];
+			  if(not find(a,i) and not find(b,i) and not find(c,i)):
 				count,row,column=count+1,j,k;
-		if(count==1):
-		   array[row][column]=i;
+		  if(count==1):
+		     array[row][column]=i;
 
-for element in array:
-    print element;
+if __name__ == "__main__":
+   sudoku()
+   for element in array:
+       print element;
 
 
