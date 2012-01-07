ba8ccbe758b8c2717af8b31fea33ad5b918291bf
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2012-01-07 10:35:29 +0530

    refactored'

--- src/45.py
+++ src/45.py
@@ -1,4 +1,16 @@
+"""
+Another way to solve without storing the results in lists is , compute the nth triangle number 
 
+and  find the roots of the below quadratic equations 
+
+3i^2-i-2x=0
+2i^2-i-x=0
+ where x is computed from i*(i+1)/2 ..
+
+If we can find the integer roots for both of these equations,then we can say that  triangle
+number x is also pentagonal and hexagonal.
+
+"""
 
 if __name__ =="__main__":
    triangle=[]
@@ -10,8 +22,6 @@ if __name__ =="__main__":
          triangle.append(t)
 	 pentagonal.append((i*(3*i-1))/2)
 	 hexagonal.append(i*(2*i-1))
-         #print t,(i*(3*i-1))/2,i*(2*i-1)
-         #print t in pentagonal
          if((t in pentagonal) and (t in hexagonal)):
              print t
              if (t!=1 and t!=40755):
